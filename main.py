c5ae9c942629ff7f7da208ed8fac663d067958af
Hugo De Carlo Rocha Filho <hugodecarlo@gmail.com>
2023-02-01 16:53:30 +0000

    Fernet encript

--- main.py
+++ main.py
@@ -2,37 +2,39 @@ import os
 import sys
 from cryptography.fernet import Fernet
 
-files = []
+# Gerando a chave
+key = Fernet.generate_key()
 
-with open("arq_teste.txt", "r") as arquivo:
-  texto = arquivo.read()
-print(texto)
+# Salvando a chave em arquivo
+with open("thekey.key", "wb") as key_file:
+    key_file.write(key)
 
-# Gera e grave chave
-chave = Fernet.generate_key()
-with open("chave_file.key", "wb") as arquivo:
-  arquivo.write(chave)
+# Criptografa o arquivo
+file_name = "file.txt"
 
-#abre arquivo
-with open("arq_teste.txt", "r") as arquivo:
-  texto = arquivo.read()
+with open(file_name, "rb") as f:
+    original_data = f.read()
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(original_data)
 
-# Criptografa e grava aquivo
-texto = Fernet(chave).encrypt(str.encode(texto))
-with open("arq_teste.txt", "wb") as arquivo:
-  arquivo.write(texto)
+# Sobrescreve o arquivo com os dados criptografados
+with open(file_name, "wb") as f:
+    f.write(encrypted_data)
 
-print("arquivo criptografado")
+# Lê a chave do arquivo
+with open("thekey.key", "rb") as key_file:
+    key = key_file.read()
+    fernet = Fernet(key)
 
-#recupera chave
-with open("chave_file.key", "rb") as arquivo:
-  chave_secreta = arquivo.read()
+# Decripta o arquivo
+with open(file_name, "rb") as f:
+    encrypted_data = f.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
 
-with open("arq_teste.txt", "r") as arquivo:
-  texto = arquivo.read()
-
-texto = Fernet(chave_secreta).decrypt(texto)
-print(texto)
-
-with open("arq_teste.txt", "w") as arquivo:
-  arquivo.write(str(texto))
+with open(file_name, "wb") as f:
+    f.write(decrypted_data)
+# Exibe o conteúdo do arquivo original, criptografado, chave e decriptografado
+print("Conteúdo original: ", original_data)
+print("Conteúdo criptografado: ", encrypted_data)
+print("Chave: ", key)
+print("Conteúdo decriptografado: ", decrypted_data)
